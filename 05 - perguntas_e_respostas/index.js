e68efcbfab98a3c5474e78984f18b4c21a59e642
evertonnatan <evertonnatandl@outlook.com>
2021-03-29 09:48:39 -0300

    Instalando o Bootstrap no projeto

--- 05 - perguntas_e_respostas/index.js
+++ 05 - perguntas_e_respostas/index.js
@@ -3,17 +3,10 @@ const app = express();
 
 // ESTOU DIZENDO PARA O EXPRESS USAR O EJS COMO VIEW ENGINE
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Google, Inc.",
-        inscritos: 8000
-    });
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
 app.listen(8080, () => {
