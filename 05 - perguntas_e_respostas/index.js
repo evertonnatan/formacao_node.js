2aeaa330a516d82810fd874f11201f226fa7e936
evertonnatan <evertonnatandl@outlook.com>
2021-04-12 09:34:12 -0300

    Capturando dados do formulario com o 'body-parser'

--- 05 - perguntas_e_respostas/index.js
+++ 05 - perguntas_e_respostas/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require("body-parser")
 // ESTOU DIZENDO PARA O EXPRESS USAR O EJS COMO VIEW ENGINE
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -11,6 +14,12 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar")
+})
+
+app.post("/salvarpergunta", (req, res) => {
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+    res.send("Formulário recebido! titulo: " + titulo + " " + "descricao: " + descricao)
 })
 
 app.listen(8080, () => {
