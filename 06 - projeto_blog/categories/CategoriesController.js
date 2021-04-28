bba42627841e4fced912f4d7eeac047dcac15d1d
evertonnatan <evertonnatandl@outlook.com>
2021-04-28 09:31:31 -0300

    Criando o formulario de categorias

--- 06 - projeto_blog/categories/CategoriesController.js
+++ 06 - projeto_blog/categories/CategoriesController.js
@@ -1,12 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get("/categories", (req, res) => {
-    res.send("Rota de categorias")
+router.get("/admin/categories/new", (req, res) =>{
+    res.render("admin/categories/new")
 })
-
-router.get("/admin/categories/new", (req, res) => {
-    res.send("Rota para criar uma nova categoria")
-})
-
 module.exports = router
