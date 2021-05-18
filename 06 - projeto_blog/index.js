4b6a0fda9bfce8ef979d281fe5cffb13bce3164e
evertonnatan <evertonnatandl@outlook.com>
2021-05-18 09:33:04 -0300

    Adicionando o jumbotron

--- 06 - projeto_blog/index.js
+++ 06 - projeto_blog/index.js
@@ -27,11 +27,13 @@ connection.authenticate().then(() => {
 })
 
 app.use("/", categoriesController)
-
 app.use("/", articlesController)
 
 app.get("/", (req, res) => {
-    res.render("index")
+
+    Article.findAll().then(articles => {
+        res.render("index", { articles: articles})
+    })
 })
 
 
